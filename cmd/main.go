2db2d6c3dc91e2cbaf46b679630d666f5e84e82d
Federico Paolinelli <fpaoline@redhat.com>
2025-09-29 12:38:17 +0200

    Main: use the nodename passed from the pod
    hostname returns hte hostname of the pod, not of the node
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -56,7 +56,6 @@ func init() {
 
 // nolint:gocyclo
 func main() {
-	var hostname string
 	var configurationRoot string
 	var metricsAddr string
 	var metricsCertPath, metricsCertName, metricsCertKey string
@@ -66,7 +65,6 @@ func main() {
 	var secureMetrics bool
 	var enableHTTP2 bool
 	var tlsOpts []func(*tls.Config)
-	flag.StringVar(&hostname, "hostname", "", "The name of the host the program will manage. Leave blank to autodetect")
 	flag.StringVar(&configurationRoot, "configuration-root", "/etc", "The root for the configuration file path")
 	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metrics endpoint binds to. "+
 		"Use :8443 for HTTPS or :8080 for HTTP, or leave as 0 to disable the metrics service.")
@@ -92,14 +90,11 @@ func main() {
 	flag.Parse()
 
 	var err error
-	if hostname == "" {
-		hostname, err = os.Hostname()
-		if err != nil {
-			setupLog.Error(err, "unable to detect the hostname")
-			os.Exit(1)
-		}
+	nodeName := os.Getenv("NODE_NAME")
+	if nodeName == "" {
+		setupLog.Error(err, "unable to detect the name of the node")
+		os.Exit(1)
 	}
-	setupLog.Info("Running for host", "hostname", hostname)
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
@@ -194,7 +189,7 @@ func main() {
 		Client:   cli,
 		Scheme:   mgr.GetScheme(),
 		ConfMgr:  configfile.NewManager(configurationRoot),
-		Labeller: nodelabel.NewManager(hostname, cli),
+		Labeller: nodelabel.NewManager(nodeName, cli),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Configuration")
 		os.Exit(1)
